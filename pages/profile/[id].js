8a0f2d67740e6b833395f1aed3e8351b60d24317
tylrk <tylerk1117@gmail.com>
2022-07-18 19:53:48 -0500

    updates

--- pages/profile/[id].js
+++ pages/profile/[id].js
@@ -1,8 +1,8 @@
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation } from "@apollo/client";
 import { useRouter } from "next/router";
 import fetchProfileQuery from "../../queries/fetchProfileQuery.js";
-
 import Profile from "../../components/Profile.js";
+import Post from "../../components/Post.js";
 
 export default function ProfilePage() {
     const router = useRouter();
@@ -10,7 +10,13 @@ export default function ProfilePage() {
 
     console.log("fetching profile for", id);
     const { loading, error, data } = useQuery(fetchProfileQuery, {
-        variables: { request: { profileId: id } },
+        variables: { 
+            request: { profileId: id },
+            publicationsRequest: {
+                profileId: id,
+                publicationTypes: ["POST"], 
+            },
+        },
     });
 
     if(loading) return "Loading...";
@@ -18,5 +24,12 @@ export default function ProfilePage() {
 
     console.log("on profile page data: ", data);
 
-    return <Profile profile={data.profile} displayFullProfile={true}/>
+    return (
+        <div className="flex flex-col p-8 items-center">
+            <Profile profile={data.profile} displayFullProfile={true} />
+            {data.publications.items.map((post, idx) => {
+                return <Post key={idx} post={post} />;
+            })}
+        </div>
+    );
 }
